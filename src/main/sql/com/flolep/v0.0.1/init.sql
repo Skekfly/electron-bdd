a51599c50aaef93898ff5b487888ca2efeca48f5
fly <mail@flolep.com>
2021-05-18 21:13:23 +0200

    Correction types BDD

--- src/main/sql/com/flolep/v0.0.1/init.sql
+++ src/main/sql/com/flolep/v0.0.1/init.sql
@@ -12,13 +12,13 @@ CREATE TABLE t_fournisseur
 
 CREATE TABLE t_releve
 (
-    rel_usr_id     NUMBER,
-    rel_fou_id     NUMBER,
+    rel_usr_id     SERIAL,
+    rel_fou_id     SERIAL,
     rel_date_debut DATE   NOT NULL,
     rel_date_fin   DATE   NOT NULL,
-    rel_hp         NUMBER NOT NULL,
-    rel_hc         NUMBER NOT NULL,
-    rel_total      NUMBER NOT NULL,
+    rel_hp         SMALLINT NOT NULL,
+    rel_hc         SMALLINT NOT NULL,
+    rel_total      SMALLINT NOT NULL,
     PRIMARY KEY (rel_usr_id, rel_fou_id),
     CONSTRAINT fk_usr FOREIGN KEY (rel_usr_id) REFERENCES t_utilisateur (usr_id),
     CONSTRAINT fk_fou FOREIGN KEY (rel_fou_id) REFERENCES t_fournisseur (fou_id)
