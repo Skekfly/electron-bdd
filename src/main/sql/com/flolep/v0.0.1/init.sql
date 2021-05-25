d52a8e6d4881c3c5fb600e99e2bf1fbd58ac6fe3
fly <mail@flolep.com>
2021-05-25 22:53:24 +0200

    changement id releve

--- src/main/sql/com/flolep/v0.0.1/init.sql
+++ src/main/sql/com/flolep/v0.0.1/init.sql
@@ -12,14 +12,14 @@ CREATE TABLE t_fournisseur
 
 CREATE TABLE t_releve
 (
-    rel_usr_id     SERIAL,
-    rel_fou_id     SERIAL,
+    rel_id         SERIAL PRIMARY KEY,
+    rel_usr_id     INTEGER,
+    rel_fou_id     INTEGER,
     rel_date_debut DATE   NOT NULL,
     rel_date_fin   DATE   NOT NULL,
     rel_hp         SMALLINT NOT NULL,
     rel_hc         SMALLINT NOT NULL,
     rel_total      SMALLINT NOT NULL,
-    PRIMARY KEY (rel_usr_id, rel_fou_id),
     CONSTRAINT fk_usr FOREIGN KEY (rel_usr_id) REFERENCES t_utilisateur (usr_id),
     CONSTRAINT fk_fou FOREIGN KEY (rel_fou_id) REFERENCES t_fournisseur (fou_id)
 );
